6f231b51d480c70eb39d239560fc314874233d55
Gabriel A. Quiles-Perez <info@gabyquiles.com>
2018-07-06 17:49:59 -0400

    Feature: Enable webpack for symfony and ReactJS

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,27 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    // the project directory where compiled assets will be stored
+// the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
     // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    // .enableBuildNotifications()
 
     // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
+    .addEntry('js/index', './assets/js/index.js')
 
     // uncomment if you use Sass/SCSS files
     // .enableSassLoader()
 
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    .enableReactPreset()
+// .configureBabel(function (babelConfig) {
+//     // add additional presets
+//     babelConfig.presets.push('stage-1');
+// })
 ;
 
 module.exports = Encore.getWebpackConfig();
