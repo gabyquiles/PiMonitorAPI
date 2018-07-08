9da00b48df2ac2a59df34fc10f20deaa5e396205
Gabriel A. Quiles-Perez <info@gabyquiles.com>
2018-07-08 13:17:19 -0400

    Misc: Clean code

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,20 +4,9 @@ namespace App\Controller;
 
 use App\Entity\ConnectionsBandwidth;
 use FOS\RestBundle\Controller\FOSRestController;
-use Symfony\Component\Routing\Annotation\Route;
 
 class ApiController extends FOSRestController
 {
-    /**
-     * @Route("/api", name="api")
-     */
-    public function index()
-    {
-        return $this->render('api/index.html.twig', [
-            'controller_name' => 'ApiController',
-        ]);
-    }
-
     public function getBandwidthAction()
     {
         $repository = $this->getDoctrine()->getRepository(ConnectionsBandwidth::class);
