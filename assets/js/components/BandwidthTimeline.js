6a34f5c9e9a3b59d76e534bf7c6f32df5364b751
Gabriel A. Quiles-Perez <info@gabyquiles.com>
2018-07-10 23:54:20 -0400

    Feature: Draw the linechart for bandwidth timeseries

--- assets/js/components/BandwidthTimeline.js
+++ assets/js/components/BandwidthTimeline.js
@@ -1,18 +1,33 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import {Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from 'recharts'
 import {getSimplifiedBandwidths} from '../selectors/bandwidths'
+import moment from 'moment'
 
 class BandwidthTimeline extends Component {
+    formatDates = (date) => {
+        return moment(date).format("HH:mm")
+    }
+
     render() {
         const {bandwidths} = this.props
         return (
-            <div>
-                <ul>
-                    {bandwidths.map((bandwidth) =>
-                        <li key={bandwidth.id}>{bandwidth.date}: {bandwidth.download} - {bandwidth.upload}</li>
-                    )}
-                </ul>
-            </div>
+            <ResponsiveContainer
+                width="100%"
+                height={500}>
+                <LineChart
+                    data={bandwidths}
+                    margin={{top: 5, right: 20, left: 10, bottom: 5}}>
+                    <Legend verticalAlign="top" height={36}/>
+                    <XAxis
+                        dataKey="date"
+                        tickFormatter={this.formatDates}/>
+                    <YAxis/>
+                    <Tooltip/>
+                    <Line type="monotone" dataKey="download" stroke="red" yAxisId={0}/>
+                    <Line type="monotone" dataKey="upload" stroke="blue" yAxisId={0}/>
+                </LineChart>
+            </ResponsiveContainer>
         )
     }
 }
